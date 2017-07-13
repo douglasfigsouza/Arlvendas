b4c8471540afafd898a4a37d500b5232ce7bf944
Douglas da Mata <douglasfigsouza@gmail.com>
2017-07-13 14:16:04 -0300

    Add project files.

---
+++ Arlvendas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arlvendas.MvcApplication" Language="C#" %>
---
+++ Arlvendas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Arlvendas.Startup))]
+namespace Arlvendas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
